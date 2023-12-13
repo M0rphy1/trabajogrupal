0415ff56c85e1e58bc31fd9a5a8dd176c29f1e70
M0rphy1 <jefemaestroasf@gmail.com>
2023-12-13 12:21:17 -0500

    Mensaje inicial, Subidos todos los archivos

--- Aplicación Web/Modelo/EliminarCliente.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("../Config/conexion.php");
-$id=$_REQUEST['cedula'];
-$sql = "DELETE from cliente where CEDULA='$id'";
-$resultado = mysqli_query($conexion,$sql);
-include("../Vista/InformacionCli.php");
-?>
--- Aplicación Web/Modelo/EliminarProducto.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../Config/conexion.php");
-$nombre=$_REQUEST['nombre'];
-$sql = "DELETE from inventario where NOMBRE='$nombre'";
-$resultado = mysqli_query($conexion,$sql);
-echo '<script language="javascript">alert("Se ha eliminado exitosamente");</script>';
-include("../Vista/ActualizarInv.php");
-?>
